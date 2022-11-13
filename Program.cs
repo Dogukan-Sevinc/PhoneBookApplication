a1e7b347ec28ecec03fdc629b653ede97b5e0771
Dogukan Sevinc <dogukansvnc13@gmail.com>
2022-11-13 18:23:56 +0300

    PhoneBookApplication

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PhoneBookApplication.Controllers;
+using PhoneBookApplication.Models;
+using System;
 
-// Doğukan Sevinç
+namespace PhoneBookApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Doğukan", "Sevinç", "123"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Özge", "Or", "456"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Kamil", "Yazıcı", "789"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Mustafa", "Yılmaz", "345"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Yasin", "Ergen", "852"));
+
+
+            OperationController.StartMethods();
+            Console.WriteLine("Çıkmak İçin Herhangi Bir Tuşa Basınız...");
+            Console.ReadKey();
+
+        }
+    }
+}
